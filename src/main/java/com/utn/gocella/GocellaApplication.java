bd1c488da47769b7befd1db9b3ef575d0b0c8c0e
lalolipc <mardelplatarocio@gmail.com>
2020-07-17 18:34:41 -0300

    create

---
+++ src/main/java/com/utn/gocella/GocellaApplication.java
@@ -0,0 +1,13 @@
+package com.utn.gocella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocellaApplication.class, args);
+	}
+
+}
